98fcb20768b41ec4715f39cd5273580da6bc963d
DENIAU Antonin <antonin.deniau@gmail.com>
2017-06-15 14:15:38 +0200

    0.3 version

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 setup(
   name = 'cave_miner',
-  packages = ['cave_miner'], # this must be the same as the name above
+  packages = ['cave_miner==0.3'], # this must be the same as the name above
   scripts = ['bin/cave_miner'],
   install_requires = ['docopt==0.6.2', 'kaitaistruct==0.7'],
-  version = '0.2',
+  version = '0.3',
   description = 'Search for code cave in all binaries',
   author = 'DENIAU Antonin',
   author_email = 'antonin.deniau@protonmail.com',
   url = 'https://github.com/Antonin-Deniau/cave_miner', # use the URL to the github repo
-  download_url = 'https://github.com/Antonin-Deniau/cave_miner/archive/0.1.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/Antonin-Deniau/cave_miner/archive/0.3.tar.gz', # I'll explain this in a second
   keywords = ['code', 'cave', 'codecave', 'macho', 'mach-o', 'elf', 'pe'], # arbitrary keywords
   classifiers = [],
 )
