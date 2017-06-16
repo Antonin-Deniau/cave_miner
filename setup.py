fad814796455a4c0d434936d8978ece88d7c5709
DENIAU Antonin <antonin.deniau@gmail.com>
2017-06-16 11:07:02 +0200

    package

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 setup(
   name = 'cave_miner',
-  packages = ['cave_miner'],
+  packages = ['cave_miner', 'cave_miner.formats'],
   entry_points={
     'console_scripts': [
       'cave_miner=cave_miner.main:main',
     ],
   },
   install_requires = ['docopt==0.6.2', 'kaitaistruct==0.7'],
-  version = '1.1',
+  version = '1.2',
   description = 'Search for code cave in all binaries',
   author = 'DENIAU Antonin',
   author_email = 'antonin.deniau@protonmail.com',
